55ace7fb22c11551b51717e23d99313a635828c9
muratayusuke <info@muratayusuke.com>
2012-10-19 09:19:35 +0900

    set GIT_SSL_NO_VERIFY

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,6 +1,7 @@
 # git env
 export PATH=/usr/local/git/bin:$PATH
 export MANPATH=/usr/local/git/share/man:`manpath -q`
+export GIT_SSL_NO_VERIFY=true
 alias g="git"
 
 fpath=($ZSH_EXT_BASE/plugins/git $fpath)
