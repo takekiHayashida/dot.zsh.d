c26d945720a5ec36c07353df1275d50cf9f276d5
muratayusuke <info@muratayusuke.com>
2012-11-11 01:00:13 +0900

    save history to file

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -45,3 +45,10 @@ alias tmux="tmux -2"
 
 # add path private scripts
 export PATH=~/.bin:$PATH
+
+# history
+HISTFILE=$HOME/.zsh-history           # 履歴をファイルに保存する
+HISTSIZE=100000                       # メモリ内の履歴の数
+SAVEHIST=100000                       # 保存される履歴の数
+setopt extended_history               # 履歴ファイルに時刻を記録
+function history-all { history -E 1 } # 全履歴の一覧を出力する
