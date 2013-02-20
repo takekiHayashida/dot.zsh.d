0018bf20775ca5c19d6118e9faf02acd583113f6
muratayusuke <info@muratayusuke.com>
2013-02-20 09:29:44 +0900

    fix emacs alias

--- lib/editor.zsh
+++ lib/editor.zsh
@@ -1,7 +1,6 @@
 # emacs config
 # alias emacs='XMODIERS="@im=none" LC_CTYPE="ja_JP.utf8" emacs 2>/dev/null'
-alias emacs='XMODIERS="@im=none" emacs 2>/dev/null'
-alias e='XMODIERS="@im=none" emacs & 2>/dev/null'
+alias e='XMODIERS="@im=none" emacs 2>/dev/null &'
 EDITOR=/usr/bin/emacsclient
 export EDITOR
 
