2204b761087155e473ea062c224e1b8381a2236d
muratayusuke <info@muratayusuke.com>
2013-03-23 22:15:04 +0900

    add ack-grep alias

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -53,3 +53,6 @@ HISTSIZE=100000                       # メモリ内の履歴の数
 SAVEHIST=100000                       # 保存される履歴の数
 setopt extended_history               # 履歴ファイルに時刻を記録
 function history-all { history -E 1 } # 全履歴の一覧を出力する
+
+# ack-grep
+alias a="ack-grep"
