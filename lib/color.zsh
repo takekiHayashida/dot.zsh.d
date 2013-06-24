d1b080b8315ef012468a42c0f3e05f36d7c9c5d6
muratayusuke <info@muratayusuke.com>
2013-06-24 16:27:13 +0000

    don't eval dircolors on mac

--- lib/color.zsh
+++ lib/color.zsh
@@ -1,7 +1,15 @@
 # プロンプトのカラー表示を有効
 autoload -U colors
 colors
+case ${OSTYPE} in
+  darwin*)
+    # for Mac
+	;;
+  *)
+    # other
+	eval `dircolors ~/.dir_colors -b`
+	;;
+esac
 
-eval `dircolors ~/.dir_colors -b`
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
