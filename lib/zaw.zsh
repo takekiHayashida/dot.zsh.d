dfe71d825e107bda65f4fa03fcb99a55b9e175bb
muratayusuke <info@muratayusuke.com>
2013-06-28 15:14:04 +0900

    move zaw settings from zaw repository

---
+++ lib/zaw.zsh
@@ -0,0 +1,8 @@
+bindkey '^gz' zaw
+bindkey '^g^b' zaw-git-recent-all-branches
+bindkey '^g^h' zaw-history
+bindkey '^gc' zaw-cdr
+bindkey '^ga' zaw-ack
+bindkey '^gs' zaw-git-status
+
+export ZAW_EDITOR='emacsclient -n'
