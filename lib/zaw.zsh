dfddfca14b3f735dcba2fe8d8aee75caa6f4a6aa
muratayusuke <info@muratayusuke.com>
2013-07-02 16:06:20 +0900

    change short-cut for zaw-history

--- lib/zaw.zsh
+++ lib/zaw.zsh
@@ -1,6 +1,6 @@
 bindkey '^gz' zaw
 bindkey '^g^b' zaw-git-recent-all-branches
-bindkey '^g^h' zaw-history
+bindkey '^r' zaw-history
 bindkey '^gc' zaw-cdr
 bindkey '^ga' zaw-ack
 bindkey '^gs' zaw-git-status
