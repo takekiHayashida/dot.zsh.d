4a54a70437b189a129ef940ad676b49ea2d9d871
muratayusuke <info@muratayusuke.com>
2013-07-07 18:35:12 +0900

    use rbenv

--- lib/ruby.zsh
+++ lib/ruby.zsh
@@ -1,8 +1,8 @@
 # ruby environment
-RUBY_HOME=/usr/local/ruby
+#RUBY_HOME=/usr/local/ruby
 
-export PATH=${RUBY_HOME}/bin:$PATH
-export MANPATH=${RUBY_HOME}/share/man:`manpath -q`
+#export PATH=${RUBY_HOME}/bin:$PATH
+#export MANPATH=${RUBY_HOME}/share/man:`manpath -q`
 
 alias rbctags="ctags --regex-ruby='/^[ \t]*([A-Z_][A-Z0-9_]*)[ \t]*=/\1/C,constant/'"
 
