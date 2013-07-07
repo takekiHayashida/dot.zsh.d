f7806a1d6ab34d9262a145c47f128896641427f5
muratayusuke <info@muratayusuke.com>
2013-07-07 19:17:56 +0900

    auto rehash when gem install

--- lib/rbenv.zsh
+++ lib/rbenv.zsh
@@ -1,2 +1,12 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+
+# auto rehash
+function gem(){
+    $HOME/.rbenv/shims/gem $*
+    if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
+    then
+        rbenv rehash
+        rehash
+    fi
+}
