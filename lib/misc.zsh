0a7257f62ce69539bbd0eefc8cd3ab3da1ede0dd
muratayusuke <info@muratayusuke.com>
2014-02-13 23:15:46 +0900

    add path to cask

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -80,3 +80,6 @@ add-zsh-hook chpwd chpwd_recent_dirs
 zstyle ':chpwd:*' recent-dirs-max 5000
 zstyle ':chpwd:*' recent-dirs-default yes
 zstyle ':completion:*' recent-dirs-insert both
+
+# cask
+export PATH="$HOME/.cask/bin:$PATH"
