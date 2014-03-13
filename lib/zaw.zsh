8677e798b1f6cf0bf4952ac5c596cc2fa931666c
muratayusuke <info@muratayusuke.com>
2014-03-13 18:33:43 +0900

    introduce zaw-ag

--- lib/zaw.zsh
+++ lib/zaw.zsh
@@ -2,7 +2,7 @@ bindkey '^gz' zaw
 bindkey '^gb' zaw-git-recent-all-branches
 bindkey '^r' zaw-history
 bindkey '^gc' zaw-cdr
-bindkey '^ga' zaw-ack
+bindkey '^ga' zaw-ag
 bindkey '^gs' zaw-git-status
 bindkey '^gf' zaw-git-files
 bindkey '^gl' zaw-git-log
