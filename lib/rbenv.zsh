36d940dddfff1a16fc13cc20a723d7d12080d72b
muratayusuke <info@muratayusuke.com>
2014-06-03 01:04:40 +0900

    load rbenv only when rbenv is installed

--- lib/rbenv.zsh
+++ lib/rbenv.zsh
@@ -1,12 +1,15 @@
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init - zsh)"
+if [ -d $HOME/.rbenv ]; then
+	export PATH="$HOME/.rbenv/bin:$PATH"
+	eval "$(rbenv init - zsh)"
 
-# auto rehash
-function gem(){
-    $HOME/.rbenv/shims/gem $*
-    if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
-    then
-        rbenv rehash
-        rehash
-    fi
-}
+	# auto rehash
+	function gem(){
+	    $HOME/.rbenv/shims/gem $*
+	    if [ "$1" = "install" ] || [ "$1" = "i" ] || [ "$1" = "uninstall" ] || [ "$1" = "uni" ]
+	    then
+	        rbenv rehash
+	        rehash
+	    fi
+	}
+fi
+
