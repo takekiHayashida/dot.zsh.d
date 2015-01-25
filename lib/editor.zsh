a55921d35349169c6ab7e2384b8cd30f2e98f570
muratayusuke <info@muratayusuke.com>
2015-01-25 18:49:31 +0900

    fix for emacs on mac

--- lib/editor.zsh
+++ lib/editor.zsh
@@ -1,5 +1,9 @@
 # emacs config
 export EDITOR=/usr/bin/emacsclient
 export ALTERNATE_EDITOR='emacs'
+if [ $OSTYPE = "darwin" ]; then
+  alias emacs='open /Applications/Emacs.app/Contents/MacOS/Emacs'
+  alias emacsclient='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n'
+fi
 alias es='XMODIERS="@im=none" emacs 2>/dev/null &'
 alias e='emacsclient -n'
