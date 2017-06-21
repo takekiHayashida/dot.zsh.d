a6db947c569379a2f05e91e91580a1ff3036a5e5
takekiHayashida <hayashida096@gmail.com>
2017-06-21 17:27:26 +0900

    Update editor.zsh

--- lib/editor.zsh
+++ lib/editor.zsh
@@ -1,9 +1,10 @@
 # emacs config
-export EDITOR=/usr/bin/emacsclient
-export ALTERNATE_EDITOR='emacs'
-if [ $OSTYPE = "darwin" ]; then
-  alias emacs='open /Applications/Emacs.app/Contents/MacOS/Emacs'
-  alias emacsclient='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n'
-fi
-alias es='XMODIERS="@im=none" emacs 2>/dev/null &'
-alias e='emacsclient -n'
+# export EDITOR=/usr/bin/emacsclient
+# export ALTERNATE_EDITOR='emacs'
+# if [ $OSTYPE = "darwin" ]; then
+#   alias emacs='open /Applications/Emacs.app/Contents/MacOS/Emacs'
+#   alias emacsclient='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n'
+# fi
+# alias es='XMODIERS="@im=none" emacs 2>/dev/null &'
+# alias e='emacsclient -n'
+alias e='atom'
